486b389267747e7d8897ad95d4ba724550a8905b
Luigi213 <reyesgianluigi88@gmail.com>
2023-02-07 11:28:44 +0100

    style optimized

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <link rel="stylesheet" href="./css/style.css">
         <title>Document</title>
     </head>
@@ -25,7 +25,7 @@
             <div class="row row-cols-1 row-cols-md-3 g-4 mb-3">
                 <?php foreach($products as $product) { ?>
                     <div class="col-auto">
-                        <div class="card pb-4">
+                        <div class="card">
                             <img src="<?php echo $product->image; ?>" class="card-img-top" alt="eighth_product">
                             <div class="card-body">
                                 <h5 class="card-title"><?php echo $product->title; ?></h5>
